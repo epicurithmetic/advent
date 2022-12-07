371b24cfa9a82a27492b3988962b00e848ac95c5
epicurithmetic <rhsculling@gmail.com>
2022-12-07 13:22:15 +1300

    Tidy up

--- 2022/day06-code.py
+++ 2022/day06-code.py
@@ -8,18 +8,17 @@ number_of_characters = 14 # Part Two: Change 4 to 14
 length_of_message = len(data)
 marker_found = False
 
-current_four = data[:14] # Part Two: Change 4 to 14
+current_block = data[:14] # Part Two: Change 4 to 14
 
 while marker_found == False:
 
-    if len(list(set(current_four))) == len(current_four):
+    if len(list(set(current_block))) == len(current_block):
         marker_found = True
-        print(current_four)
+        print(current_block)
         print(number_of_characters)
         
     else:
-        current_four = current_four[1:] + list(data[number_of_characters])
+        current_block = current_block[1:] + list(data[number_of_characters])
 
 
     number_of_characters += 1
-
