c1f1b866c781f5d51367b990c0535f4fcdf43028
epicurithmetic <rhsculling@gmail.com>
2022-12-09 19:33:40 +1300

    Grab the data for day09

---
+++ 2022/day09-code.py
@@ -0,0 +1,5 @@
+# Advent of Code 2022 :: Day 09
+head_movements_file = open("day09-data.txt","r")
+head_movements = head_movements_file.read()
+head_movements_file.close()
+head_movements = head_movements.split("\n")
